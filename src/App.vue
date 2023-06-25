da4f4376e3a1dbf6e42c24d21c10ae7bf32bad75
abuliegeguolage <123>
2023-06-25 17:04:35 +0800

    裝了less和less-loader，還在App.vue加了一堆註釋，你之後會恨我的>wo

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,20 @@
 </script>
 
 <template>
-  <!-- 這裡可以放他媽的header -->
-  <!-- 這樣之後就不用他媽的每個文件都打一遍header的碼 -->
+  <!-- router-view前面可以放header -->
+  <!-- 這樣之後就不用每個文件都打一遍header的碼 -->
   <router-link to="/">首頁</router-link>
   <router-link to="/outside">另一頁</router-link>
+  <!-- ↑這個標籤之後會被自動替換成a標籤，網址不會外連 -->
+  <!-- router-link會把to的路徑接在網址上，route資料夾的index.js會根據路徑吐出對應的文件 -->
   <router-view></router-view>
+  <!-- ↑這個標籤會被自動替換成使用者當前的頁面組件 -->
 </template>
 
 <style scoped>
+/* 
+這裡通常會放全域的樣式，所以之後記得幹掉scoped
+*/
   a {
     margin: 10px;
   }
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,3 +1,7 @@
 <template>
     <h1>Home</h1>
 </template>
+
+<style lang="less" scoped>
+    //有了less和less-loader就可以在vue組件裡寫巢狀css啦！
+</style>
